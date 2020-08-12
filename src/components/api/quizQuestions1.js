2b83de50ea5a90a38d9bb30f450613c398bece84
brookebachman00 <bbachman1994@gmail.com>
2020-08-11 20:28:59 -0600

    I have a winner shown, still need to use localStorage

---
+++ src/components/api/quizQuestions1.js
@@ -0,0 +1,196 @@
+// const quizQuestions = [
+// 	{
+// 		question: 'In what year was Sega Genesis released in North America?',
+answers:[]
+// 		1989: true,
+// 		1999: false,
+// 		1975: false,
+// 		1991: false,
+// 	},
+
+// 	{
+// 		question: 'Which of the following video games takes place in a dystopian underwater city called Rapture?',
+// 		Bioshock: true,
+// 		'Half-Life': false,
+// 		'God Of War': false,
+// 		'Fallout 3': false,
+// 	},
+
+// 	{
+// 		question: 'What Nintendo system was released after the N64 and before the Wii?',
+// 		Gamecube: true,
+// 		'Nintendo 128': false,
+// 		'Virtual Boy': false,
+// 		'Super Nintendo': false,
+// 	},
+
+// 	{
+// 		question: 'Honda, Dhalsim and Chun Li are all characters from what video game series?',
+// 		'Street Fighter': true,
+// 		'Teenage Mutant Ninja Turtles': false,
+// 		Battletoads: false,
+// 		'Mortal Kombat': false,
+// 	},
+
+// 	{
+// 		question: 'What color is the ring of death on an XBOX that signifies a hardware failure?',
+// 		Red: true,
+// 		Blue: false,
+// 		Yellow: false,
+// 		Green: false,
+// 	},
+
+// 	{
+// 		question:
+// 			"What classic beat-'em-up game featured brothers Billy Lee and Jimmy (also nicknamed Spike and Hammer)?",
+// 		'Double Dragon': true,
+// 		'Smash Brothers': false,
+// 		'Ninja Gaiden': false,
+
+// 		'Snow Brothers': false,
+// 	},
+
+// 	{
+// 		question: 'How many bits was the Super Nintendo Entertainment System?',
+
+// 		16: true,
+// 		8: false,
+// 		128: false,
+// 		64: false,
+// 	},
+
+// 	{
+// 		question: 'What character do you play as in The Legend Of Zelda?',
+// 		Link: true,
+// 		Gandolf: false,
+// 		Chimmy: false,
+// 		Peter: false,
+// 	},
+
+// 	{
+// 		question: 'What 64-bit Sega system was a predecessor to the PlayStation and Nintendo 64?',
+// 		Saturn: true,
+// 		Dreamcast: false,
+// 		'Commodore 128': false,
+// 		'3DO': false,
+// 	},
+
+// 	{
+// 		question: 'The game Grand Theft Auto was released primarily for what gaming system?',
+// 		PlayStation: true,
+// 		Dreamcast: false,
+// 		XBOX: false,
+// 		NES: false,
+// 	},
+
+// 	{
+// 		question:
+// 			'Which James Bond film was made into a game for the Nintendo 64 console and later for the Wii console?',
+// 		Goldeneye: true,
+// 		Thunderball: false,
+// 		Moonraker: false,
+// 		Goldfinger: false,
+// 	},
+
+// 	{
+// 		question:
+// 			'Which popular video game features an ex-Special Forces operator named Jack Carver, who is stranded in Micronesia?',
+// 		'Far Cry': true,
+// 		'Halo 2': false,
+// 		'Left 4 Dead': false,
+// 		'Max Payne': false,
+// 	},
+// ];
+
+// export default quizQuestions;
+
+
+var quizQuestions = [
+    {
+        question: "What franchise would you rather play a game from?",
+        answers: [
+            {
+                type: "Microsoft",
+                content: "Halo"
+            },
+            {
+                type: "Nintendo",
+                content: "Pokemon"
+            },
+            {
+                type: "Sony",
+                content: "Uncharted"
+            }
+        ]
+    },
+    {
+        question: "Which console would you prefer to play with friends?",
+        answers: [
+            {
+                type: "Microsoft",
+                content: "X-Box"
+            },
+            {
+                type: "Nintendo",
+                content: "Nintendo 64"
+            },
+            {
+                type: "Sony",
+                content: "Playstation 1"
+            }
+        ]
+    },
+    {
+        question: "Which of these racing franchises would you prefer to play a game from?",
+        answers: [
+            {
+                type: "Microsoft",
+                content: "Forza"
+            },
+            {
+                type: "Nintendo",
+                content: "Mario Kart"
+            },
+            {
+                type: "Sony",
+                content: "Gran Turismo"
+            }
+        ]
+    },
+    {
+        question: "Which of these games do you think is best?",
+        answers: [
+            {
+                type: "Microsoft",
+                content: "BioShock"
+            },
+            {
+                type: "Nintendo",
+                content: "The Legend of Zelda: Ocarina of Time"
+            },
+            {
+                type: "Sony",
+                content: "Final Fantasy VII"
+            }
+        ]
+    },
+    {
+        question: "What console would you prefer to own?",
+        answers: [
+            {
+                type: "Microsoft",
+                content: "X-Box One"
+            },
+            {
+                type: "Nintendo",
+                content: "Wii U"
+            },
+            {
+                type: "Sony",
+                content: "Playstation 4"
+            }
+        ]
+    }
+  ];
+  
+  export default quizQuestions;
