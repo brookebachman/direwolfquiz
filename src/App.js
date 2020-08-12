57275717f76c02b4aa4c06da83babf22a8457242
brookebachman00 <bbachman1994@gmail.com>
2020-08-11 18:18:25 -0600

    fixed the quiz questions to match mine

--- src/App.js
+++ src/App.js
@@ -126,7 +126,7 @@ import Result from './components/Result.js'
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>React Quiz</h2>
+          <h2>So you think you know Gaming?</h2>
         </div>
         {this.state.result ? this.renderResult() : this.renderQuiz()}
       </div>
