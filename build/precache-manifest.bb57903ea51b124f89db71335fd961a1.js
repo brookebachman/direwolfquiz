f01d9fa5a900622bda8235f30917dca4a05ea7db
brookebachman00 <bbachman1994@gmail.com>
2020-08-17 17:10:56 -0600

    Public folder

---
+++ build/precache-manifest.bb57903ea51b124f89db71335fd961a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9927bacd6b22df9fd373d410cfd62c04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fa75d29b3d5550d644b",
+    "url": "/static/css/main.a9f1143e.chunk.css"
+  },
+  {
+    "revision": "1ded84ef93dc4e847fb8",
+    "url": "/static/js/2.fefdab69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fefdab69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fa75d29b3d5550d644b",
+    "url": "/static/js/main.dfeccb0b.chunk.js"
+  },
+  {
+    "revision": "06a5b8f01ce735c40972",
+    "url": "/static/js/runtime-main.ea9983c2.js"
+  },
+  {
+    "revision": "67bf5c4e903a119b358e6db814b235c4",
+    "url": "/static/media/logo.67bf5c4e.jpg"
+  }
+]);
